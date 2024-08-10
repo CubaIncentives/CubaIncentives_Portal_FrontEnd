00fff0b83284a5b7f76b65b99b15e6404f9f4046
CubaIncentives <131451765+CubaIncentives@users.noreply.github.com>
2024-08-10 22:01:26 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 # Script to run pnpm install and pnpm build
 
-# Run pnpm install
-source ~/.bashrc
+# Load environment variables
+. ~/.bashrc
 
+# Run pnpm install
 echo "Running pnpm install..."
-pnpm install
+pnpm install || { echo "pnpm install failed"; exit 1; }
 
 # Run pnpm build
 echo "Running pnpm build..."
-pnpm build
+pnpm build || { echo "pnpm build failed"; exit 1; }
 
 # Output completion message
 echo "Installation and build completed successfully!"
 
-# Copy all files from dist to ~/public_html/
-echo "Delete all files in public directory"
-rm -rf ~/public_html/dev-portal.cubaincentives.com/*
+# Check if the dist directory exists
+if [ ! -d "dist" ]; then
+  echo "dist directory does not exist. Exiting..."
+  exit 1
+fi
 
-# Copy all files from dist to ~/public_html/
-echo "Copying files from dist to public_html"
-cp -nrf dist/* ~/public_html/dev-portal.cubaincentives.com
+# Delete old files and copy new files
+BASENAME=$(basename "$PWD")
+
+  echo "Deleting all files"
+  echo "Copying files from dist"
+if [ "$BASENAME" = "CubaIncentives_Portal_BackEnd" ]; then
+  rm -rf ~/public_html/dev-backend.cubaincentives.com/*
+  cp -nrf dist/* ~/public_html/dev-portal.cubaincentives.com
+elif [ "$BASENAME" = "Prod_CubaIncentives_Portal_BackEnd" ]; then
+  rm -rf ~/public_html/staging-backend.cubaincentives.com/*
+  cp -nrf dist/* ~/public_html/staging-portal.cubaincentives.com
+fi
+
+echo "Files copied successfully!"
